b553057f29dfa62954ddff8be2e56ee6d06cb4b8
Raúl López Hernando <raullopezhernando@gmail.com>
2019-10-14 09:40:19 +0200

    Bucle FOREACH , Metodos de List ....

---
+++ List/List/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List <string> paises = new List <string>()
+            {
+
+                "Francia","Holanda", "Inglaterra", "Polonia"
+
+            };
+
+            paises.Add("Italia");
+            paises.Insert(8, "Austria");
+            paises.RemoveAt(3);
+            paises.Remove("Holanda");
+            //paises[2]; // Te da una posicion determinada
+
+            int posicion = paises.IndexOf("Francia"); // Te busca la primera coincidencia del string . En este caso Francia. Si no encuentra -1
+
+
+            // Recorrer una lista . Normalmente con un For
+
+            for (int i = 0; i < paises.Count; i++)
+            {
+
+                Console.WriteLine(paises[i]);
+            }
+
+
+            // BUCLE FOREACH
+
+            // Por cada elemento en la lista de paises {haz esto}. Recorre toda la lista . NO PERSONALIZABLE
+
+            foreach (string elemento in paises)
+            {
+                Console.WriteLine(elemento);
+            }
+
+           
+
+        }
+    }
+}
